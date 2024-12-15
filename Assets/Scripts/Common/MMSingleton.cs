394cf9f1e5a281bc5a6e06adc2388f97879cf9c2
lizy <eagle.lzy@gmail.com>
2024-12-15 14:28:10 +0800

    新增单例类

---
+++ Assets/Scripts/Common/MMSingleton.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MMSingleton<T> : MonoBehaviour where T : Component
+{
+    public static T Current => _instance;
+    protected static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null)
+                {
+                    GameObject obj = new();
+                    obj.name = typeof(T).Name + "_AutoCreated";
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        InitializeSingleton();
+    }
+
+    protected virtual void InitializeSingleton()
+    {
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            if (this != _instance)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
