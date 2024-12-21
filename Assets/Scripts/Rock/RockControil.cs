a383e1ce61c7678c2e3da4d041665f446a2e3942
navither <482033387@qq.com>
2024-12-21 13:43:48 +0800

    加点原画

---
+++ Assets/Scripts/Rock/RockControil.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockControil : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float force = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(1, 1) * force;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
