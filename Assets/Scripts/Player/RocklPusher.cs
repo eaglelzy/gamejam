fa59213705ceb8f5f796d89890a2841e21e61d75
BTP <3503922521@qq.com>
2024-12-23 12:46:34 +0800

    玩家可以用身体推石头

---
+++ Assets/Scripts/Player/RocklPusher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TS.Player {
+
+    public class RocklPusher : MonoBehaviour {
+        public Collider2D pushCollider;
+        public ContactFilter2D contactFilter;
+        public float force = 100;
+        private static readonly List<Collider2D> _cache = new List<Collider2D>();
+
+        private void FixedUpdate() {
+            var count = pushCollider.OverlapCollider(contactFilter, _cache);
+            for (int i = 0; i < count; ++i) {
+                if (_cache[i].gameObject.CompareTag("Rock")) {
+                    _cache[i].attachedRigidbody.AddForce(force * pushCollider.transform.right);
+                    break;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Rock/RockControl.cs
+++ Assets/Scripts/Rock/RockControl.cs
@@ -2,33 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RockControl : MonoBehaviour
-{
+public class RockControl : MonoBehaviour {
 
     [Tooltip("初始下降速度")]
     [SerializeField]
     private float force = 10;
 
-
     private Rigidbody2D rb;
-    
 
     // Start is called before the first frame update
-    void Start()
-    {
+    private void Start() {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(1, 1) * force;
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-/*        if (collision.gameObject.CompareTag("Bullet"))
-        {
-            rb.AddForce(scrollDirection * bulletForce, ForceMode2D.Impulse);
-            if (rb.velocity.x > maxSpeed)
-            {
-                rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
-            }
-        }*/
     }
 }
