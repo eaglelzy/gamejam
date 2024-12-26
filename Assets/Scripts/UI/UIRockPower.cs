1a34f2bba82d03342b86f742cad90da2c77212c1
BTP <3503922521@qq.com>
2024-12-26 12:07:39 +0800

    修复身体推石头功能，添加石头能量条渐变色

--- Assets/Scripts/UI/UIRockPower.cs
+++ Assets/Scripts/UI/UIRockPower.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIRockPower : MonoBehaviour
-{
-    //石头能量UI
-    private Image powerImage;
+public class UIRockPower : MonoBehaviour {
+    public Gradient gradient;  //能量条颜色渐变
 
-    private void Awake() {
-        powerImage = GetComponent<Image>();
-    }
+    private Image powerImage;  //石头能量UI
 
     public void SetPower(float power) {
         powerImage.fillAmount = power / 100;
+        powerImage.color = gradient.Evaluate(powerImage.fillAmount);
+    }
+
+    private void Awake() {
+        powerImage = GetComponent<Image>();
     }
 
     private void Update() {
